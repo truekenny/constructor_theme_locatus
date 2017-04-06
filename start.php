1c87fed381244d8ebcc220ca485395ecd8d1cc18
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2017-04-06 18:52:40 +0300

    Реализация блока .header и модуля search

---
+++ start.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+    <link rel="stylesheet" type="text/css" href="bem/body/body.css">
+    <link rel="stylesheet" type="text/css" href="bem/header/header.css">
+    <link rel="stylesheet" type="text/css" href="bem/logo/logo.css">
+    <link rel="stylesheet" type="text/css" href="bem/title/title.css">
+    <link rel="stylesheet" type="text/css" href="bem/link/link.css">
+    <link rel="stylesheet" type="text/css" href="bem/subtitle/subtitle.css">
+    <link rel="stylesheet" type="text/css" href="bem/module-search/module-search.css">
+    <link rel="stylesheet" type="text/css" href="bem/input/input.css">
+    <link rel="stylesheet" type="text/css" href="bem/button/button.css">
+</head>
+<body class="body">
+
+<div class="header">
+    <div class="header__background"></div>
+
+    <div class="logo header__logo">
+        <a href="#" class="logo__link"></a>
+    </div>
+
+    <div class="title header__title">
+        <h1 class="title__text">
+            Поиск организаций и услуг в
+            <a href="#" class="link_color_green link_decorate_dotted title__link">Москве</a>
+        </h1>
+    </div>
+
+    <div class="subtitle header__subtitle">
+        <h2 class="subtitle__text">
+            Среди 10 200 организаций и 57 000 услуг
+        </h2>
+    </div>
+
+    <div class="module-search header__module-search">
+        <form class="module-search__form">
+            <input class="input input_text-size_big module-search__input">
+            <button class="button button_color_green button_icon_search module-search__button"></button>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
